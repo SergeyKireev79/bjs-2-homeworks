38dc0f7e0b03fa769ce0269dbf39462927eefb01
Sergey Kireev <de-griiz@yandex.ru>
2021-09-27 10:28:53 +0500

    Задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result;
+    if (arr1.every((item, i) => item === arr2[i]) && arr1.length == arr2.length) {
+        result = true
+    } else result = false;
+    return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+    let resultArr = arr.filter((item) => item >= 0 && item % 3 === 0)
+        .map((item) => item * 10)
+    return resultArr;
 }
