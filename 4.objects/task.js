2eeba48bcf584137d87563e1146190a144ffe86d
Sergey Kireev <de-griiz@yandex.ru>
2021-09-30 00:33:57 +0500

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name,
+        this.gender = gender,
+        this.age = age
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName
 }
 
-// ваш код для остальных методов
+
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark]
+    } else {
+        this.marks.push(mark)
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+    this.marks = marks
+}
+
+
+Student.prototype.getAvg = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return avg = ((sum / arr.length).toFixed(4))
+}
+
+Student.prototype.exclude = function(reason) {
+    this.excluded = reason
+    delete this.marks
+    delete this.subject
+}
