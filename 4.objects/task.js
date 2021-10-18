e1f7de0bc1bf8ecc972ee618d5888f854a7b5d49
Sergey Kireev <de-griiz@yandex.ru>
2021-10-18 17:33:10 +0500

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -20,6 +20,8 @@ Student.prototype.addMark = function(mark) {
 Student.prototype.addMarks = function(...marks) {
     if (this.marks === undefined) {
         this.marks = marks
+    } else {
+        this.marks.push(marks)
     }
 }
 
